73373e05ab7d4d2d8d163f0ab16b014ca3f52738
KaioWillamis <brm560548@gmail.com>
2024-09-16 15:25:56 -0300

    Atualizei

--- principal.php
+++ principal.php
@@ -15,22 +15,22 @@ include "sessao.php";
     <main>
         <div onclick="window.location.href='curso.php'" class="curso">
             <p>Cursos</p>
-            <img src="" alt="">
+            <img src="img/curso.png" alt="vetor curso" width="115px" height="115px">
         </div>
 
         <div onclick="window.location.href='matricula.php'" class="matricula">
             <p>Matrícula</p>
-            <img src="" alt="">
+            <img src="img/matricula.png" alt="vetor matrícula" width="80px" height="114px">
         </div> 
 
         <div onclick="window.location.href='alunos.php'" class="alunos">
             <p>Alunos</p>
-            <img src="" alt="">
+            <img src="img/aluno.png" alt="vetor aluno" width="130px" height="130px">
         </div>
     
         <div onclick="window.location.href='login.php'" class="voltar">
             <p>Voltar</p>
-            <img src="" alt="">
+            <img src="img/voltar.png" alt="vetor seta para voltar" width="130px" height="130px">
         </div>
     </main>
 </body>
